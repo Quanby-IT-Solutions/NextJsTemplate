4a06617c1f0168456616084ed0147a71ed4beed3
joemar25 <joemar.cardino000@gmail.com>
2024-09-14 00:14:28 +0800

    config check

--- src/_codux/boards/code-block/code-block.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createBoard } from "@wixc3/react-board";
-import { CodeBlock } from "../../../components/code-block/CodeBlock";
-
-export default createBoard({
-  name: "CodeBlock",
-  Board: () => <CodeBlock code={`const hello = 'world';`} />,
-  isSnippet: true,
-});
