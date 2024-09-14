a84f9c821347f570e74815521aca76aa0bcade6b
joemar25 <joemar.cardino000@gmail.com>
2024-09-14 09:49:11 +0800

    component searching

--- src/components/dashboad/dashboad.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface DashboadProps {
-  className?: string;
-}
-
-export const Dashboad: React.FC<DashboadProps> = ({ className = "" }) => (
-  <div className={className}>Dashboad</div>
-);
