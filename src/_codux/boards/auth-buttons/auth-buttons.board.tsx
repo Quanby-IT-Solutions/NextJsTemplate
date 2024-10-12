d3691ac35e09e8f2d28dd37e520acaa133b1fe59
joemar25 <joemar.cardino000@gmail.com>
2024-10-12 13:14:36 +0800

    deleted boards

--- src/_codux/boards/auth-buttons/auth-buttons.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createBoard } from "@wixc3/react-board";
-import { AuthButtons } from "../../../components/auth-button/AuthButton";
-
-export default createBoard({
-  name: "AuthButtons",
-  Board: () => <AuthButtons />,
-  isSnippet: true,
-});
--- src/_codux/boards/checkbox/checkbox.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from "@wixc3/react-board";
-import { Checkbox } from "../../../components/checkbox/Checkbox";
-
-export default createBoard({
-  name: "Checkbox",
-  Board: () => <Checkbox className="custom-checkbox-class" />,
-  isSnippet: true,
-});
